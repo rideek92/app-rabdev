14e0168953b7392b7df374f5c9e4a2deeb4b9b0f
Damian Rabiński <damian.rabinski@gmail.com>
2023-05-16 18:15:05 +0200

    create socialmedia section

--- src/components/molecules/social/SocialMedia.js
+++ src/components/molecules/social/SocialMedia.js
@@ -1,27 +1,43 @@
 import Link from 'next/link';
 import SVGIcon from '@/components/svgicon/svgicon';
-import classes from './socialmedia.module.sass';
+import { icon, wrapper } from './socialmedia.module.sass';
 
 const socials = [
   {
-    name: 'GitHub',
-    url: './assets/icons/GitHub.svg',
+    name: 'Twitter',
+    url: './assets/icons/Twitter.svg',
+    address: 'https://twitter.com/Damian70464506',
+    active: true,
+  },
+  {
+    name: 'Linkedin',
+    url: './assets/icons/Linkedin.svg',
+    address: 'https://www.linkedin.com/in/damian-rabi%C5%84ski-07b265113/',
+    active: true,
+  },
+  {
+    name: 'Instagram',
+    url: './assets/icons/Instagram.svg',
     address: 'https://github.com/rideek92',
+    active: false,
   },
   {
     name: 'GitHub',
     url: './assets/icons/GitHub.svg',
     address: 'https://github.com/rideek92',
+    active: true,
   },
 ];
 export default function SocialMedia() {
   return (
-    <>
-      {socials.map((social) => (
-        <Link href={social.address} target="_blank">
-          <SVGIcon image={social.url} classElement={classes.icon} />
-        </Link>
-      ))}
-    </>
+    <div className={wrapper}>
+      {socials.map((social) =>
+        social.active ? (
+          <Link href={social.address} target="_blank">
+            <SVGIcon image={social.url} classElement={icon} />
+          </Link>
+        ) : null
+      )}
+    </div>
   );
 }
